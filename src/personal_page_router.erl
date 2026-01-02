c0c0782fed835cba6fed7dff62827d2709db76d8
Mark Chizhov <mail@benpuls.ru>
2026-01-02 19:57:16 +0500

    remove: api and blog routes

--- src/personal_page_router.erl
+++ src/personal_page_router.erl
@@ -5,7 +5,6 @@
 -export([routes/1]).
 
 -define(MAIN, personal_page_main_controller).
--define(API, personal_page_api_controller).
 
 section(Prefix, Security, Routes) ->
     #{prefix => Prefix,
@@ -24,20 +23,6 @@ main() ->
              route("/blog", ?MAIN, blog, [get]),
              route("/post:id", ?MAIN, post, [get])]).
 
-api() ->
-    section("/api",
-            {personal_page_auth, auth_user},
-            [route("/healthcheck", ?API, healthcheck, [get]),
-             route("/post/:id", ?API, get_post, [get]),
-             route("/post/:id", ?API, insert_post, [post]),
-             route("/post/:id", ?API, update_post, [put]),
-             route("/post/:id", ?API, delete_post, [delete])]).
-
-other() ->
-    #{}.
-
 %% The Environment-variable is defined in your sys.config in {nova, [{environment, Value}]}
 routes(_Environment) ->
-    [main(), api()].
-
-% TODO: add blog routes (some routes may be protected by auth module)
+    [main()].
